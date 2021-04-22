2aaddfb3434e4fa2d2d1648c28eff800f5bcd5d9
mynorbeats <82728782+mynorbeats@users.noreply.github.com>
2021-04-22 18:14:55 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function f1() {
+    document.getElementById('output1').value=+document.getElementById('input1.1').value+ +document.getElementById('input1.2').value;
+}
+function f2() {
+    document.getElementById('output2').value=+document.getElementById('input2.1').value- +document.getElementById('input2.2').value;
+}
+function f3() {
+    document.getElementById('output3').value=+document.getElementById('input3.1').value* +document.getElementById('input3.2').value;
+}
+function f4() {
+    document.getElementById('output4').value=+document.getElementById('input4.1').value/ +document.getElementById('input4.2').value;
+}
